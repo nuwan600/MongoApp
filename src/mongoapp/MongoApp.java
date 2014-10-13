1979e4a661deac621e5d2ba58aabd4705408036c
nuwan600 <nuwan600@gmail.com>
2014-10-14 02:29:07 +0530

    MongoApp

---
+++ src/mongoapp/MongoApp.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mongoapp;
+
+/**
+ *
+ * @author nuwan600
+ */
+public class MongoApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NewJFrame.main(args);
+       //Main.main(args);
+    }
+}
